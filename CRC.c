c6a63159e1859930b1e32def88c5da0947494952
MXNXV-ERR <MXNXV-ERR@users.noreply.github.com>
2022-09-19 22:30:35 +0530

    refined

--- CRC.c
+++ CRC.c
@@ -1,45 +1,46 @@
+#include<stdio.h>
 #include<string.h>
-#define N strlen(g)
-char t[28],cs[28],g[28];
-int a,e,c,b;
-void xor()
+#define N strlen(divisor)
+char data[28],temp[28],divisor[28];
+int l,i,j;
+void xorl()
 {
-        for(c=1;c<N;c++)
-                cs[c]=((cs[c]==g[c])?'0':'1');
+        for(j=1;j<N;j++)
+                temp[j]=((temp[j]==divisor[j])?'0':'1');
 }
 void crc()
 {
-        for(e=0;e<N;e++)
-                cs[e]=t[e];
+        for(i=0;i<N;i++)
+                temp[i]=data[i];
         do{
-                if(cs[0]=='1')
-                        xor();
-                for(c=0;c<N-1;c++)
-                        cs[c]=cs[c+1];
-                cs[c]=t[e++];
-        }while(e<=a+N-1);
+                if(temp[0]=='1')
+                        xorl();
+                for(j=0;j<N-1;j++)
+                        temp[j]=temp[j+1];
+                temp[j]=data[i++];
+        }while(i<=l+N-1);
 }
 int main()
 {
         printf("Enter the data\n");
-        scanf("%s",&t);
+        scanf("%s",&data);
         char ch='y';
         printf("CRC-16?(y/n)");
         scanf(" %c",&ch);
         if(ch=='y')
-                strncpy(g, "1100000000000101", 28);
+                strncpy(divisor, "1100000000000101", 28);
         else
         {       printf("Enter the divisor\n");
-                scanf("%s",&g);
+                scanf("%s",&divisor);
         }
-        printf("\nGenerating polynomial:%s\n",g);
-        a=strlen(t);
-        for(e=a;e<a+N-1;e++)
-                t[e]='0';
-        printf("Modified data is:%s\n",t);
+        printf("\nGenerating polynomial:%s\n",divisor);
+        l=strlen(data);
+        for(i=l;i<l+N-1;i++)
+                data[i]='0';
+        printf("Modified data is:%s\n",data);
         crc();
-        printf("Remainder is:%s\n",cs);
-        for(e=a;e<a+N-1;e++)
-                t[e]=cs[e-a];
-        printf("\nFinal codeword is : %s\n",t);
+        printf("Remainder is:%s\n",temp);
+        for(i=l;i<l+N-1;i++)
+                data[i]=temp[i-l];
+        printf("\nFinal codeword is : %s\n",data);
 }
